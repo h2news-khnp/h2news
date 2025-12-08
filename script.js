d9778a259e7f6c10a519cb9614c41bb1e84f76a8
h2news-khnp <likemkay@gmail.com>
2025-12-09 01:00:02 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// 앞으로 여기에 JSON 파일(크롤러 출력)을 불러와서
+// 화면에 뉴스 카드를 그리는 코드를 넣을 예정입니다.
+
+// 예시: 나중에 이런 식으로 쓸 예정
+// fetch('data/2025-12-09.json')
+//   .then(res => res.json())
+//   .then(renderNewsList);
+
+console.log("수소 뉴스 아카이브: script.js 로드됨");
